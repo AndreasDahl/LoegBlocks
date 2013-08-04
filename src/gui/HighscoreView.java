197731c65365289a9037bfa645b30d5edce86ff7
Andreas Dahl <andreas@hrdahl.dk>
2013-08-04 14:37:06 +0200

    added new HighscoreView to HighscoresMenu

---
+++ src/gui/HighscoreView.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.util.LinkedList;
+
+import model.DbScoreboard;
+import model.Timer;
+import view.Art;
+import view.Screen;
+
+public class HighscoreView extends GuiComponent {
+	private int scoresShown = 10;
+	private LinkedList<Long> scores;
+	
+	public HighscoreView(int x, int y, int width, int height) {
+		super(x , y, width, height);
+		
+		refresh();
+	}
+	
+	public void refresh() {
+		scores = new DbScoreboard().getTop(scoresShown);
+	}
+	
+	@Override
+	public void render(Screen screen) {
+		super.render(screen);
+		
+		screen.renderBlank(getX(), getY(), getWidth(), getHeight(), 0xffffffff);
+		
+		int dist = getHeight()/scoresShown;
+		int i = 0;
+		for (Long score : scores) {
+			Art.FONT.render(getX(), getY()+(dist*i), dist, Timer.longToTimeString(score), screen);
+			i++;
+		}
+	}
+}
--- src/gui/HighscoresMenu.java
+++ src/gui/HighscoresMenu.java
@@ -6,12 +6,16 @@ import view.Screen;
 import gui.ButtonListener;
 
 public class HighscoresMenu extends GuiMenu {
+	private Button backButton;
+	private HighscoreView highscoreView;
 	
 	public HighscoresMenu() {
 		super();
-		Button backButton = new Button(getWidth()/2-150, GameFrame.HEIGHT-100, 300, 100, Art.BUTTON, "Back");
+		backButton = new Button(getWidth()/2-100, getHeight()-75, 200, 50, Art.BUTTON, "Back");
 		backButton.addButtonListener(new BackListener());
 		addButton(backButton);
+		
+		highscoreView = new HighscoreView(50, 50, getWidth()-100, getHeight()-200);
 	}
 	
 	class BackListener implements ButtonListener {
@@ -24,6 +28,8 @@ public class HighscoresMenu extends GuiMenu {
 	@Override
 	public void render(Screen screen) {
 		screen.renderBlank(getX(), getY(), getX()+getWidth(), getY()+getHeight(), 0xff007f7f);
+		
+		highscoreView.render(screen);
 		
 		super.render(screen);
 	}
